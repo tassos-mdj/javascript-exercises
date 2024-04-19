7f69992d104c3048a744137b175dac8c1dc2c6fc
tassos-mdj <164147506+tassos-mdj@users.noreply.github.com>
2024-04-19 19:21:55 +0300

    Complete 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function( string, num ) {
+    let finalString = '';
+if (num < 0) { 
+    finalString = 'ERROR'
+} else {
+    for (let i = 0 ; i < num ; i++) {
+    finalString = finalString + string;
+    }
+}
+return finalString;
 };
 
 // Do not edit below this line
