2bb675139865f7006aefb17274d5182c4256543a
tassos-mdj <164147506+tassos-mdj@users.noreply.github.com>
2024-04-19 20:48:55 +0300

    Complete 04_removeFromArray

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
-
+const reverseString = function(initialString) {
+    let i = initialString.length-1;
+    let newString = '';
+    while (i > -1){
+        newString += initialString[i];
+        i--;
+    }
+    return newString;
 };
 
 // Do not edit below this line
--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,13 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(userArray, ...elementsToRetrieve) {
+    for (let element of elementsToRetrieve) {
+        for (let i = 0 ; i < userArray.length+1 ; i++) {
+            if (userArray[i] === element) {
+            userArray.splice(i,1);
+            i--;
+            }
+        }
+    }
+return userArray;
 };
 
 // Do not edit below this line
