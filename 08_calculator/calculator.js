851847661517469807215e00651fb81f3d445cbc
tassos-mdj <164147506+tassos-mdj@users.noreply.github.com>
2024-05-14 16:36:03 +0300

    Start excercise 8

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,21 +1,35 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+	return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function(...args) {
+	if (arguments.length === 0) { return 0;} else {
+    for (let arg in args) {
+      let sum = sum + arg;
+    }
+    return sum;
+  } 
 };
 
-const multiply = function() {
-
+const multiply = function(...args) {
+	if (arguments.length === 0) { return 0;} else {
+    for (let arg in args) {
+      let sum = sum + arg;
+    }
+    return sum;
+  } 
 };
 
-const power = function() {
-	
+const power = function(num1, num2) {
+	let sum = 1;
+  for (let i = 0; i < num2; i++) {
+    sum = sum * num1;
+  }
+  return sum;
 };
 
 const factorial = function() {
