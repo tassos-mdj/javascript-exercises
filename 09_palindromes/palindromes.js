bef4c73323b3bc7b94b1e08255074360c0a7d08b
tassos-mdj <164147506+tassos-mdj@users.noreply.github.com>
2024-05-14 20:48:36 +0300

    Work on exc 9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,7 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    str.toLowerCase().replace(/[^a-z]/gi, '');
+    let revStr = str.split("").reverse().join("");
+    return str === revStr;
 };
 
 // Do not edit below this line
