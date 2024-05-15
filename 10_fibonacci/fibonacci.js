3fb6283a06cf219459d360cc512087792a58d70b
tassos-mdj <164147506+tassos-mdj@users.noreply.github.com>
2024-05-15 23:42:10 +0300

    complete exc 10_fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,12 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    const intNum = Number(num);
+    if (intNum < 0) { return "OOPS";}
+    else {
+        if (intNum === 0 || intNum === 1) {return intNum;}
+        else {
+            return (fibonacci(intNum -1)) + (fibonacci(intNum -2));
+        }
+    }
 };
 
 // Do not edit below this line
