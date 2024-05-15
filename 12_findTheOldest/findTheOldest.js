a3efcc8d9dd815f3785a2c163b47a5f0cbc67b0c
tassos-mdj <164147506+tassos-mdj@users.noreply.github.com>
2024-05-16 01:35:46 +0300

    complete 12 find the oldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,20 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    let oldest = people[0];
+    
+    for (let i = 0; i < people.length; i++) {
+        let current = people[i];
+        if (getAge(current["yearOfBirth"], current["yearOfDeath"]) > getAge(oldest["yearOfBirth"], oldest["yearOfDeath"])) {
+            oldest = current;
+        }
+    }
+    return oldest;
 };
 
+function getAge(birth, death) {
+    if (!death) {
+        death = new Date().getFullYear();
+      }
+      return death - birth;
+}
 // Do not edit below this line
 module.exports = findTheOldest;
