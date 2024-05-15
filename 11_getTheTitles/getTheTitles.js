9cf795a6e588beb4ceb5436ce7001f4c0848c057
tassos-mdj <164147506+tassos-mdj@users.noreply.github.com>
2024-05-16 00:07:46 +0300

    complete exc 11_getTheTitles

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,6 +1,12 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(books) {
+    const titles = [];
+     for (let i = 0; i < books.length; i++){
+        titles[i] = books[i]["title"];
+    }
+    return titles;
+    
 };
+
 
 // Do not edit below this line
 module.exports = getTheTitles;
